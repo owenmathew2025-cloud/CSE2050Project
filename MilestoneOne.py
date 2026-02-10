3e010fb34de0a8ba41134ea910ea97031c41db05
owenmathew2025-cloud <owenmathew2025@gmail.com>
2026-02-10 10:22:18 -0500

    Update MilestoneOne.py

--- MilestoneOne.py
+++ MilestoneOne.py
@@ -1,1 +1,11 @@
-pass
+class Course:
+  def init(self, course_code, credits, students):
+    self.course_code = course_code
+    self.credits = credits
+    self.students = students
+    
+  def add_student(self, student):
+    pass
+  
+  def get_student_count(self):
+    pass
